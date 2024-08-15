da29cba2bb16b9196dae6d216b9309d10d8ea8d5
Dantesys <hotuios@hotmail.com>
2024-08-15 19:57:38 -0300

    v2.2.1

--- src/main/java/me/dantesys/valentCity/events/ReliquiasEvent.java
+++ src/main/java/me/dantesys/valentCity/events/ReliquiasEvent.java
@@ -1,6 +1,5 @@
 package me.dantesys.valentCity.events;
 
-import io.papermc.paper.event.player.PlayerPickItemEvent;
 import me.dantesys.valentCity.Temporizador;
 import me.dantesys.valentCity.ValentCity;
 import me.dantesys.valentCity.items.Reliquias;
@@ -124,20 +123,6 @@ public class ReliquiasEvent implements Listener {
         }
     }
     @EventHandler
-    public static void punicao(PlayerPickItemEvent event){
-        Player player = event.getPlayer();
-        int slot = event.getSourceSlot();
-        if(!player.getName().equals("HeriteHunter")){
-            ItemStack item = player.getInventory().getItem(slot);
-            if(item != null && item.isSimilar(Reliquias.heritehunter)){
-                player.getInventory().remove(item);
-                player.updateInventory();
-                player.getWorld().dropItem(player.getLocation(),item);
-                player.sendMessage("Você não é HeriteHunter");
-            }
-        }
-    }
-    @EventHandler
     public static void punicao(PlayerInteractEvent event){
         Player player = event.getPlayer();
         ItemStack item = event.getItem();
@@ -159,9 +144,40 @@ public class ReliquiasEvent implements Listener {
                 final Location location = player.getLocation();
                 final Vector direction = location.getDirection().normalize();
                 final double[] tp = {0};
+                AtomicReference<ItemStack> cap = new AtomicReference<>();
+                AtomicReference<ItemStack> pei = new AtomicReference<>();
+                AtomicReference<ItemStack> cal = new AtomicReference<>();
+                AtomicReference<ItemStack> bot = new AtomicReference<>();
+                Temporizador timer2 = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
+                        ()->{
+                            alvo[0].showPlayer(ValentCity.getPlugin(ValentCity.class),player);
+                            alvo[0].getInventory().setHelmet(null);
+                            alvo[0].getInventory().setChestplate(null);
+                            alvo[0].getInventory().setLeggings(null);
+                            alvo[0].getInventory().setBoots(null);
+                            alvo[0].sendMessage("Maldição Aplicada!");
+                            alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS,20,1));
+                            player.sendMessage("Maldição Aplicada!");
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS,20,1));
+                        },
+                        ()->{
+                            alvo[0].hidePlayer(ValentCity.getPlugin(ValentCity.class),player);
+                            alvo[0].getInventory().setHelmet(cap.get());
+                            alvo[0].getInventory().setChestplate(pei.get());
+                            alvo[0].getInventory().setLeggings(cal.get());
+                            alvo[0].getInventory().setBoots(bot.get());
+                        },
+                        (t)->{
+                            alvo[0].sendMessage("Sobreviva por "+t.getSegundosRestantes()+"segundos!");
+                            player.sendMessage("Se esconda por "+t.getSegundosRestantes()+"segundos!");
+                        });
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                    cap.set(alvo[0].getInventory().getHelmet());
+                    pei.set(alvo[0].getInventory().getChestplate());
+                    cal.set(alvo[0].getInventory().getLeggings());
+                    bot.set(alvo[0].getInventory().getBoots());
+                        },()->{timer2.scheduleTimer(20L);},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -183,39 +199,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    final Player armor = alvo[0];
-                    ItemStack cap = armor.getInventory().getHelmet();
-                    ItemStack pei = armor.getInventory().getChestplate();
-                    ItemStack cal = armor.getInventory().getLeggings();
-                    ItemStack bot = armor.getInventory().getBoots();
-                    Temporizador timer2 = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
-                            ()->{
-                                armor.showPlayer(ValentCity.getPlugin(ValentCity.class),player);
-                                armor.getInventory().setHelmet(null);
-                                armor.getInventory().setChestplate(null);
-                                armor.getInventory().setLeggings(null);
-                                armor.getInventory().setBoots(null);
-                                armor.sendMessage("Maldição Aplicada!");
-                                armor.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS,20,1));
-                                player.sendMessage("Maldição Aplicada!");
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS,20,1));
-                            },
-                            ()->{
-                                armor.hidePlayer(ValentCity.getPlugin(ValentCity.class),player);
-                                armor.getInventory().setHelmet(cap);
-                                armor.getInventory().setChestplate(pei);
-                                armor.getInventory().setLeggings(cal);
-                                armor.getInventory().setBoots(bot);
-                            },
-                            (t)->{
-                                armor.sendMessage("Sobreviva por "+t.getSegundosRestantes()+"segundos!");
-                                player.sendMessage("Se esconda por "+t.getSegundosRestantes()+"segundos!");
-                            });
-                    timer2.scheduleTimer(20L);
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb2 != null && hb2.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),600);
@@ -226,7 +209,11 @@ public class ReliquiasEvent implements Listener {
                 final double[] tp = {0};
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{
+                    alvo[0].sendMessage("Cisco no olho!");
+                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,100,1));
+                    alvo[0].damage(2);
+                    },(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -248,13 +235,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    alvo[0].sendMessage("Cisco no olho!");
-                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,100,1));
-                    alvo[0].damage(2);
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb3 != null && hb3.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),600);
@@ -265,7 +245,10 @@ public class ReliquiasEvent implements Listener {
                 final double[] tp = {0};
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{
+                    alvo[0].sendMessage("Que frio!");
+                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS,100,10));
+                    alvo[0].setFreezeTicks(alvo[0].getMaxFreezeTicks()+100);},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -287,13 +270,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    alvo[0].sendMessage("Que frio!");
-                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS,100,10));
-                    alvo[0].setFreezeTicks(alvo[0].getMaxFreezeTicks()+100);
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb4 != null && hb4.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),600);
@@ -304,7 +280,10 @@ public class ReliquiasEvent implements Listener {
                 final double[] tp = {0};
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{
+                    alvo[0].sendMessage("Que calor!");
+                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.GLOWING,100,10));
+                    alvo[0].setFireTicks(alvo[0].getMaxFireTicks()+100);},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -326,13 +305,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    alvo[0].sendMessage("Que calor!");
-                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.GLOWING,100,10));
-                    alvo[0].setFireTicks(alvo[0].getMaxFireTicks()+100);
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb5 != null && hb5.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),400);
@@ -343,7 +315,8 @@ public class ReliquiasEvent implements Listener {
                 final double[] tp = {0};
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{alvo[0].sendMessage("Gavidade 0!");
+                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,100,10));},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -365,12 +338,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    alvo[0].sendMessage("Gavidade 0!");
-                    alvo[0].addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,100,10));
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb6 != null && hb6.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),1000);
@@ -381,7 +348,12 @@ public class ReliquiasEvent implements Listener {
                 final double[] tp = {0};
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{alvo[0].sendMessage("Insetos!");
+                    Location l = alvo[0].getLocation();
+                    World w = alvo[0].getWorld();
+                    w.spawn(l,Silverfish.class);
+                    w.spawn(l,Silverfish.class);
+                    w.spawn(l,Silverfish.class);},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -403,16 +375,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    alvo[0].sendMessage("Insetos!");
-                    Location l = alvo[0].getLocation();
-                    World w = alvo[0].getWorld();
-                    w.spawn(l,Silverfish.class);
-                    w.spawn(l,Silverfish.class);
-                    w.spawn(l,Silverfish.class);
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb7 != null && hb7.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),1000);
@@ -423,7 +385,11 @@ public class ReliquiasEvent implements Listener {
                 final double[] tp = {0};
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{alvo[0].sendMessage("Onde é que eu estou!");
+                    Location l = alvo[0].getLocation();
+                    Location l2 = player.getLocation();
+                    alvo[0].teleport(l2);
+                    player.teleport(l);},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -445,15 +411,6 @@ public class ReliquiasEvent implements Listener {
                     }
                 });
                 timer.scheduleTimer(5L);
-                if(alvo[0]!=null){
-                    alvo[0].sendMessage("Onde é que eu estou!");
-                    Location l = alvo[0].getLocation();
-                    Location l2 = player.getLocation();
-                    alvo[0].teleport(l2);
-                    player.teleport(l);
-                }else{
-                    player.setCooldown(Reliquias.heritehunter.getType(),0);
-                }
             }
             else if(hb8 != null && hb8.isSimilar(Reliquias.heritehunter)){
                 player.setCooldown(Reliquias.heritehunter.getType(),400);
@@ -462,9 +419,22 @@ public class ReliquiasEvent implements Listener {
                 final Location location = player.getLocation();
                 final Vector direction = location.getDirection().normalize();
                 final double[] tp = {0};
+                AtomicReference<Location> l = new AtomicReference<>();
+                AtomicReference<World> w = new AtomicReference<>();
+                AtomicReference<Creeper> c = new AtomicReference<>();
+                Temporizador timer2 = new Temporizador(ValentCity.getPlugin(ValentCity.class), 2,
+                        ()-> {c.set(w.get().spawn(l.get(), Creeper.class));
+                            c.get().setPowered(true);
+                        },()-> c.get().remove(),
+                        (t)->alvo[0].playSound(l.get(),Sound.ENTITY_CREEPER_PRIMED, 3.0f,0.5f)
+                );
+
                 Temporizador timer = new Temporizador(ValentCity.getPlugin(ValentCity.class), 10,
                         ()->{
-                        },()->{},(t)->{
+                        },()->{alvo[0].sendMessage("Aw man!");
+                    l.set(alvo[0].getLocation());
+                    w.set(alvo[0].getWorld());
+                    timer2.scheduleTimer(20L);},(t)->{
                     tp[0] = tp[0]+3.4;
                     double x = direction.getX()*tp[0];
                     double y = direction.getY()*tp[0]+1.4;
@@ -487,17 +457,8 @@ public class ReliquiasEvent implements Listener {
                 });
                 timer.scheduleTimer(5L);
                 if(alvo[0]!=null){
-                    alvo[0].sendMessage("Aw man!");
-                    Location l = alvo[0].getLocation();
-                    World w = alvo[0].getWorld();
-                    AtomicReference<Creeper> c = new AtomicReference<>();
-                    Temporizador timer2 = new Temporizador(ValentCity.getPlugin(ValentCity.class), 2,
-                            ()-> {c.set(w.spawn(l, Creeper.class));
-                            c.get().setPowered(true);
-                            },()-> c.get().remove(),
-                            (t)->alvo[0].playSound(l,Sound.ENTITY_CREEPER_PRIMED, 3.0f,0.5f)
-                    );
-                    timer2.scheduleTimer(20L);
+
+
                 }else{
                     player.setCooldown(Reliquias.heritehunter.getType(),0);
                 }
